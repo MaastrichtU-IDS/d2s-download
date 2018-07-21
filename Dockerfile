71739b1430ef22ded905608fd6d26b13287cc017
vemonet <vincent.emonet@gmail.com>
2018-07-21 04:16:17 +0200

    redirect fails to download_failed.log. Fix install. Now fully working

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:18.04
 
 RUN apt-get update -y
-RUN apt-get install -y apt-utils rename gzip unzip 
+RUN apt-get install -y apt-utils rename gzip unzip wget
 
 COPY datasets/ /datasets/
 COPY download_datasets.sh /download_datasets.sh
--- download_datasets.sh
+++ download_datasets.sh
@@ -8,7 +8,7 @@ mkdir -p $DDL_WORKDIR
 all_datasets=( "affymetrix" "clinicaltrials" "ctd" "dbpedia" "drugbank" "genage" "gendr" "genbank" "goa" "hgnc" 
   "interpro" "homologene" "ipi" "irefindex" "kegg" "lsr" "mgi" "ncbigene" "pharmgkb" "pubchem" "refseq" "sider" "taxonomy" "uniprot" "wormbase" )
 
-download_datasets=( "wormbase" )
+download_datasets=( "drugbank" )
 
 for dataset in "${download_datasets[@]}"
 do
@@ -18,5 +18,11 @@ do
   source datasets/${dataset}/download.sh $DATASET_DIR
 done
 
+cd $DDL_WORKDIR
+echo "  Failed downloads" > download_failed.log
+
 # Go through the logs to check for error or failed download
-grep -rl 'failed.*ERROR\|ERROR.*failed' $DDL_WORKDIR > $DDL_WORKDIR/download.log
+grep -rl ERROR >> download_failed.log
+grep -rl failed >> download_failed.log
+echo "yeaaah"
+grep -rl "*ERROR*"
