997f239b667b08895e6a571e9c30f76cff8a0087
vemonet <vincent.emonet@gmail.com>
2018-07-28 02:35:56 +0200

    fixed bindingdb

--- datasets/bindingdb/download.sh
+++ datasets/bindingdb/download.sh
@@ -3,13 +3,20 @@ if [[ -z "$1" ]]; then
   echo "Provide a target directory to store downloaded files as argument. E.g.: /data/kraken-download/datasets"
   exit 1
 fi
-
 mkdir -p $1
 cd $1
 rm -rf *
 
 # BindingDB https://www.bindingdb.org/bind/chemsearch/marvin/SDFdownload.jsp?all_download=yes
 wget -a download.log https://www.bindingdb.org/bind/downloads/BindingDB_All_2018m6.tsv.zip
+wget -a download.log https://www.bindingdb.org/bind/chemsearch/marvin/SDFdownload.jsp?all_download=yes
+
+array=( $(cat index.html | sed -r -n 's/.*>(BindingDB_All_[^"]*?.\.tsv\.zip).*/\1/p') )
+
+#<li><a href="/bind/chemsearch/marvin/SDFdownload.jsp?download_file=/bind/downloads/BindingDB_All_2018m6.tsv.zip">BindingDB_All_2018m6.tsv.zip</a> ( 254.56 MB, updated 2018-07-01 )</li>
+wget -a download.log ${myarr[0]}
+
+#https://www.bindingdb.org/bind/downloads/BindingDB_All_2018m6.tsv.zip
 
 # Unzip all files in subdir with name of the zip file
 find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`/${filename%.*}" "$filename"; done;
