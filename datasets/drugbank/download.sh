85902c935c44f3466cfa8e5145555fc8cd781631
vemonet <vincent.emonet@gmail.com>
2018-09-21 00:52:24 +0200

    fix drugbank ddl (with auth, using curl)

--- datasets/drugbank/download.sh
+++ datasets/drugbank/download.sh
@@ -8,30 +8,8 @@ mkdir -p $1
 cd $1
 rm -rf *
 
-# Log in to the server. This only needs to be done once.
-wget -a download.log --save-cookies cookies.txt \
-     --keep-session-cookies \
-     --post-data 'public_user[email]=$drugbank_login&public_user[password]=$drugbank_password&public_user[remember_me]=1&commit=Signin' \
-     https://www.drugbank.ca/public_users/sign_in
-     #--delete-after \
-# Seems to work better when credentials are hard coded (see cookies.txt)...
-
-wget -a download.log --save-cookies cookies.txt \
-     --keep-session-cookies \
-     --post-data 'public_user[email]=vincent.emonet@maastrichtuniversity.nl&public_user[password]=maasitest12&public_user[remember_me]=1&commit=Signin' \
-     https://www.drugbank.ca/public_users/sign_in
-
-# Download all extracted links
-wget -a download.log -O drugbank_all_full_database.xml.zip --load-cookies cookies.txt "https://drugbank.s3.us-west-2.amazonaws.com/public_downloads/downloads/000/002/022/original/drugbank_all_full_database.xml.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJTZC3DSCEEG75A6Q%2F20180719%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180719T003719Z&X-Amz-Expires=10&X-Amz-SignedHeaders=host&X-Amz-Signature=9fb60bf8ac341884d1b00375e23d576902834d2735ddc14d1ed08f588c12b186"
-# GENERATE SIGNATURE? http://blog.0xpebbles.org/shell-script-for-S3-upload-via-curl-using-AWS-version-4-signatures
-
-
-wget -a download.log -O drugbank_all_full_database.xml.zip --load-cookies cookies.txt "https://drugbank.s3.us-west-2.amazonaws.com/public_downloads/downloads/000/002/022/original/drugbank_all_full_database.xml.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJTZC3DSCEEG75A6Q%2F20180719%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180719T003719Z&X-Amz-Expires=10&X-Amz-SignedHeaders=host"
-
-wget -O drugbank_all_full_database.xml.zip --load-cookies cookies.txt "https://drugbank.s3.us-west-2.amazonaws.com/public_downloads/downloads/000/002/022/original/drugbank_all_full_database.xml.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=1060e3ab56c3e0715f96ead254ca3784bf276d92%2F20180719%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180719T003719Z&X-Amz-Expires=10&X-Amz-SignedHeaders=host&X-Amz-Signature=de6acc03ffbf47c74775d4832dba044ae1532447964"
-
-
-# Returns 403 Forbidden
+# Download providing user login and password
+curl -Lfv -o drugbank.zip -u $drugbank_login:$drugbank_password https://www.drugbank.ca/releases/5-1-1/downloads/all-full-database
 
 # Unzip all files in subdir with name of the zip file
 find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`/${filename%.*}" "$filename"; done;
