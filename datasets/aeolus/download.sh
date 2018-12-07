7eaf0d73607459c973753cb5f18eb01a8691ffa3
vemonet <vincent.emonet@gmail.com>
2018-12-07 23:17:59 +0100

    add ddl aeolus

---
+++ datasets/aeolus/download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+  echo "Provide a target directory to store downloaded files as argument. E.g.: /data/download"
+  exit 1
+fi
+mkdir -p $1
+cd $1
+rm -rf *
+
+########## Download files
+
+wget -a download.log https://datadryad.org/bitstream/handle/10255/dryad.105332/aeolus_v1.zip
+
+########## UNCOMPRESS
+
+# ZIP
+# Recursive in subdir
+find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`/${filename%.*}" "$filename"; done;
