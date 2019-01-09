3604ea8509e10b5fdacde60d0bd4a9113bc7457c
vemonet <vincent.emonet@gmail.com>
2019-01-09 13:03:35 +0100

    now ddl only protein.actions tsv file for STRING db. and download it in 3 different version to test temporal querying

--- datasets/string/download.sh
+++ datasets/string/download.sh
@@ -10,17 +10,28 @@ rm -rf *
 
 ### String DB: https://string-db.org/cgi/download.pl
 
-# full database, part I: the players (proteins, species, COGs,...). 4.9G
-wget -a download.log https://stringdb-static.org/download/items_schema.v10.5.sql.gz
+# Download multiple version of the TSV protein.actions containing protein interactions
 
-# full database, part II: the networks (nodes, edges, scores,...). 41.3G
-wget -a download.log https://stringdb-static.org/download/network_schema.v10.5.sql.gz
+# Download latest version 10.5
+wget -a download.log https://stringdb-static.org/download/protein.actions.v10.5/9606.protein.actions.v10.5.txt.gz
 
-# full database, part III: interaction evidence (but: excluding license-restricted data). 6.9G
-wget -a download.log https://stringdb-static.org/download/evidence_schema.v10.5.sql.gz
+# Download previous version 9.1 and 10
+wget -a download.log http://version10.string-db.org/download/protein.actions.v10/9606.protein.actions.v10.txt.gz
+wget -a download.log http://string91.embl.de/newstring_download/protein.actions.v9.1/9606.protein.actions.v9.1.txt.gz
 
-# full database, part IV: homology data (all-against-all SIMAP similarity searches). 460G
-wget -a download.log https://stringdb-static.org/download/homology_schema.v10.5.sql.gz
+
+### Old way: download whole SQL database, but too big (more than 500G)
+# players (proteins, species, COGs,...). 4.9G
+#wget -a download.log https://stringdb-static.org/download/items_schema.v10.5.sql.gz
+# networks (nodes, edges, scores,...). 41.3G
+#wget -a download.log https://stringdb-static.org/download/network_schema.v10.5.sql.gz
+# interaction evidence (but: excluding license-restricted data). 6.9G
+#wget -a download.log https://stringdb-static.org/download/evidence_schema.v10.5.sql.gz
+# homology data (all-against-all SIMAP similarity searches). 460G
+#wget -a download.log https://stringdb-static.org/download/homology_schema.v10.5.sql.gz
+
 
 
 find . -name "*.gz" -exec gzip -d  {} +
+
+rename s/\.txt/.tsv/ *.txt
