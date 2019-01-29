497cdab99f8d2f9febfaddcb239338496844f94e
vemonet <vincent.emonet@gmail.com>
2019-01-29 09:27:43 +0100

    new download link for Kegg as RDF

--- datasets/kegg/download.sh
+++ datasets/kegg/download.sh
@@ -8,21 +8,25 @@ mkdir -p $1
 cd $1
 rm -rf *
 
-BASE_URI="http://rest.kegg.jp/list/"
-array=( "pathway" "disease" "drug" "compound" "genome" "genes" "enzyme" "reaction" "ko" "module" "environ" "glycan" "rclass" )
+# RDF dump 2G from https://www.genome.jp/oc/
+wget -a download.log https://www.genome.jp/oc/files/oc_rdf_170605.tar.gz
+
+find . -name "*.tar.gz" -exec tar -xzvf {} \;
+
+
+#BASE_URI="http://rest.kegg.jp/list/"
+#array=( "pathway" "disease" "drug" "compound" "genome" "genes" "enzyme" "reaction" "ko" "module" "environ" "glycan" "rclass" )
 
 # Get all IDs to download
-for database in "${array[@]}"
-do
-  echo "Downloading database... ${database}"
-  wget -a download.log $BASE_URI$database -O "$database.html"
-
+#for database in "${array[@]}"
+#do
+#  echo "Downloading database... ${database}"
+#  wget -a download.log $BASE_URI$database -O "$database.html"
   # Download each ID as seq file
-  ids=( $(cat $database.html | sed -r -n 's/(gn:.*?)[\s].*/\1/p') )
-
-  for id in "${ids[@]}"
-  do
-    echo "Downloading ID... ${id}"
-    wget -a download.log http://rest.kegg.jp/get/#{id}
-  done
-done
+#  ids=( $(cat $database.html | sed -r -n 's/(gn:.*?)[\s].*/\1/p') )
+#  for id in "${ids[@]}"
+#  do
+#    echo "Downloading ID... ${id}"
+#    wget -a download.log http://rest.kegg.jp/get/#{id}
+#  done
+#done
