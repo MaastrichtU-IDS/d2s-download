f8b02f0c2092b4ac7a45c464b8ae1ced1cb229ab
vemonet <vincent.emonet@gmail.com>
2019-03-23 01:30:56 +0100

    create pubmed sample to test pipeline and fix pubmed zip

--- datasets/pubmed-sample/download.sh
+++ datasets/pubmed-sample/download.sh
@@ -7,9 +7,10 @@ mkdir -p $1
 cd $1
 rm -rf *
 
-wget -a download.log -r -A xml.gz -nH --cut-dirs=1 ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/
+#wget -a download.log -r -A xml.gz -nH --cut-dirs=1 ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/
 
-#wget -a download.log -r -A xml.gz -nH --cut-dirs=1 ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/
+wget -a download.log ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed19n0001.xml.gz
+wget -a download.log ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed19n0002.xml.gz
 
 # Unzip all files in subdir with name of the zip file
 #find . -name "*.gz" -exec gzip -d  {} +
