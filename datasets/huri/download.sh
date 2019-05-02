ed1540006ed0002223119f00f89a884cd08bebc5
vemonet <vincent.emonet@gmail.com>
2019-05-02 02:35:28 +0200

    add ddl script for huri tsv

---
+++ datasets/huri/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+  echo "Provide a target directory to store downloaded files as argument. E.g.: /data/download"
+  exit 1
+fi
+mkdir -p $1
+cd $1
+rm -rf *
+
+# HuRI: http://interactome.baderlab.org/download
+
+wget -a download.log http://interactome.baderlab.org/data/HuRI.tsv
