e983d492a7a0746e46807153ea755e64c10a507b
vemonet <vincent.emonet@gmail.com>
2019-08-13 19:43:51 +0200

    add drugbank sample for test

---
+++ datasets/drugbank-sample/download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+  echo "Provide a target directory to store downloaded files as argument. E.g.: /data/kraken-download/datasets"
+  exit 1
+fi
+mkdir -p $1
+cd $1
+rm -rf *
+
+wget -a download.log https://raw.githubusercontent.com/MaastrichtU-IDS/data2services-download/master/datasets/drugbank-sample/drugbank-sample.xml
+
+# Download providing user login and password
+#curl -Lfv -o drugbank.zip -u $USERNAME:$PASSWORD https://www.drugbank.ca/releases/5-1-1/downloads/all-full-database
+
+# Also directly available as RDF here: http://wifo5-03.informatik.uni-mannheim.de/drugbank/
+
+# Unzip all files in subdir with name of the zip file
+#find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`/${filename%.*}" "$filename"; done;
