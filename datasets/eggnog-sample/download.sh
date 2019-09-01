277843f194cd098cb60acfeacae056745c67665d
vemonet <vincent.emonet@gmail.com>
2019-09-02 00:35:10 +0200

    add eggnog-sample

---
+++ datasets/eggnog-sample/download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+  echo "Provide a target directory to store downloaded files as argument. E.g.: /data/download"
+  exit 1
+fi
+mkdir -p $1
+cd $1
+rm -rf *
+
+
+### EggNOG
+
+# http://eggnogdb.embl.de/#/app/downloads
+
+wget -a download.log http://eggnogdb.embl.de/download/latest/data/NOG/NOG.members.tsv.gz
+
+wget -a download.log http://eggnogdb.embl.de/download/latest/data/NOG/NOG.annotations.tsv.gz
+
+#wget -a download.log http://eggnogdb.embl.de/download/latest/data/NOG/NOG.trees.tsv.gz
+
+
+# Only FASTA sequences (.fa or .hmm files)
+#wget -a download.log http://eggnogdb.embl.de/download/eggnog_4.5/data/NOG/NOG.trimmed_algs.tar.gz
+#wget -a download.log http://eggnogdb.embl.de/download/eggnog_4.5/data/NOG/NOG.raw_algs.tar.gz
+#wget -a download.log http://eggnogdb.embl.de/download/eggnog_4.5/data/NOG/NOG.hmm.tar.gz
+
+find . -name "*.gz" -exec gzip -d  {} +
+
+# Add columns name
+sed -i '1s/^/TaxonomicLevel\tGroupName\tProteinCount\tSpeciesCount\tCOGFunctionalCategory\tProteinIDs\n/' NOG.members.tsv
+sed -i '1s/^/TaxonomicLevel\tGroupName\tProteinCount\tSpeciesCount\tCOGFunctionalCategory\tConsensusFunctionalDescription\n/' NOG.annotations.tsv
